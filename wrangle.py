64620719a339925e1b6582de667c0cfce326c91a
Lori Ainslie <lori.ainslie18@gmail.com>
2021-12-03 18:02:21 -0500

    updating with additional completed work and functions

--- wrangle.py
+++ wrangle.py
@@ -74,9 +74,9 @@ def remove_outliers(df, k, col_list):
     return df
 
 
-def prepare_zillow_data(df):
+def clean_zillow_data(df):
     '''
-    This function takes in the zillow data, cleans it, splits it, and returns train, validate & test dataframes
+    This function takes in the zillow data, cleans it, and returns a dataframe
     '''
     
     # Rename some columns for simplicity
@@ -85,6 +85,11 @@ def prepare_zillow_data(df):
                             'taxvaluedollarcnt':'taxvalue'})
     # Apply a function to remove outliers
     df = remove_outliers(df, 1.5, ['bedrooms', 'bathrooms','area','taxvalue','taxamount'])
+    
+    # Remove more of the outliers for area
+    df = df[(df.area > 500) & (df.area < 2500)]
+    # Remove more of the outliers for taxvalue
+    df = df[(df.taxvalue > 500) & (df.taxvalue < 800000)]
     
     # Drop rows with null values since it is only a small portion of the dataframe 
     df = df.dropna()
@@ -100,10 +105,38 @@ def prepare_zillow_data(df):
         if col in obj_col_list:
             df[col] = df[col].astype(int).astype(object)
     
-    # Split the data into train, validate, and test
-    train_validate, test = train_test_split(df, test_size=.2, random_state=369)
-    train, validate = train_test_split(train_validate, test_size=.3, random_state=369)
+    # drop taxamount since we will be predicting tax value and tax amount is considered data leakage
+    df = df.drop(columns='taxamount')
+    
+    return df
 
+
+########## Split ##########
+
+# Split the data into train, validate, and test
+def split_data(df, random_state=369, stratify=None):
+    '''
+    This function takes in a dataframe and splits the data into train, validate and test samples. 
+    Test, validate, and train are 20%, 24%, & 56% of the original dataset, respectively. 
+    The function returns train, validate and test dataframes.
+    '''
+   
+    if stratify == None:
+        # split dataframe 80/20
+        train_validate, test = train_test_split(df, test_size=.2, random_state=random_state)
+
+        # split larger dataframe from previous split 70/30
+        train, validate = train_test_split(train_validate, test_size=.3, random_state=random_state)
+    else:
+
+        # split dataframe 80/20
+        train_validate, test = train_test_split(df, test_size=.2, random_state=random_state, stratify=df[stratify])
+
+        # split larger dataframe from previous split 70/30
+        train, validate = train_test_split(train_validate, test_size=.3, 
+                            random_state=random_state,stratify=train_validate[stratify])
+
+    # results in 3 dataframes
     return train, validate, test
 
 
@@ -111,6 +144,6 @@ def prepare_zillow_data(df):
 
 def wrangle_zillow():
     '''This function acquires, cleans, and splits data from the zillow database for exploration'''
-    train, validate, test = prepare_zillow_data(get_zillow_data())
+    train, validate, test = split_data(clean_zillow_data(get_zillow_data()))
     
     return train, validate, test
